7c6d648e69d40dc91910fda0dcddd3616701b001
Shivani-Kamath <shivanikamath369@gmail.com>
2025-05-21 18:29:03 +0530

    Update and rename ArrayListP1.java to ColorSearchArrayList.java

--- 1b.ArrayList/ColorSearchArrayList.java
+++ 1b.ArrayList/ColorSearchArrayList.java
@@ -2,10 +2,10 @@
  1. Write a java program for getting different colors through ArrayList interface and search whether
  the color "Red" is available or not */
 
-package AdvancedJava;
+package ArrayList;
 import java.util.*;
 
-public class ArrayListP1 {
+public class ColorSearchArrayList {
 	public static void main(String[] args) {
 		
 		List<String> ls = new ArrayList<>();
